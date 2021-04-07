8c59a02f342b0b238eebbdbf5d428b05776538af
Akash Kava <ackava@gmail.com>
2021-04-07 14:43:59 +0530

    Added attachments support

--- MailDispatcher/Controllers/DispatchController.cs
+++ MailDispatcher/Controllers/DispatchController.cs
@@ -27,34 +27,7 @@ namespace MailDispatcher.Controllers
             var a = await accountRepository.GetAsync(id);
             if (a.AuthKey != auth)
                 return Unauthorized();
-            MimeMessage msg = new MimeMessage();
-            msg.From.Add(MailboxAddress.Parse(model.From));
-            List<string> recipients 
-                = new List<string>(
-                    (model.To?.Length ?? 0) +
-                    (model.Cc?.Length ?? 0) +
-                    (model.Bcc?.Length ?? 0));
-            if (recipients.Capacity == 0)
-                return BadRequest("Need at least one recipient");
-            foreach(var to in model.To) {
-                msg.To.Add(MailboxAddress.Parse(to));
-                recipients.Add(to);
-            }
-            if (model.Cc != null) {
-                foreach (var to in model.Cc) {
-                    msg.Cc.Add(MailboxAddress.Parse(to));
-                    recipients.Add(to);
-                }
-            }
-            if (model.Bcc != null) {
-                foreach (var to in model.Bcc) {
-                    msg.Bcc.Add(MailboxAddress.Parse(to));
-                    recipients.Add(to);
-                }
-            }
-            var ms = new MemoryStream();
-            msg.WriteTo(ms);
-            ms.Seek(0, SeekOrigin.Begin);
+            var (ms, recipients) = model.ToMessage(Request.Form.Files);
             var r = await jobs.Queue(id, new RawMessageRequest { 
                 From = model.From,
                 Recipients = recipients.ToArray()
--- MailDispatcher/Storage/SimpleMail.cs
+++ MailDispatcher/Storage/SimpleMail.cs
@@ -1,18 +1,108 @@
-﻿namespace MailDispatcher.Storage
+﻿#nullable enable
+using Microsoft.AspNetCore.Http;
+using MimeKit;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+
+namespace MailDispatcher.Storage
 {
     public class SimpleMail
     {
-        public string From { get; set; }
-        public string[] To { get; set; }
+        public string? From { get; set; }
+        public string[]? To { get; set; }
 
-        public string[] Cc { get; set; }
+        public string[]? Cc { get; set; }
 
-        public string[] Bcc { get; set; }
+        public string[]? Bcc { get; set; }
 
-        public string Subject { get; set; }
+        public string? ReplyTo { get; set; }
 
-        public string TextBody { get; set; }
+        public string? Subject { get; set; }
 
-        public string HtmlBody { get; set; }
+        public string? Summary { get; set; }
+
+        public string? TextBody { get; set; }
+
+        public string? HtmlBody { get; set; }
+
+        public string? UnsubscribeLink { get; set; }
+
+        public (Stream stream, List<string> recipients) ToMessage(IFormFileCollection? files)
+        {
+            if (From == null)
+                throw new ValidationException("Need from");
+            if (Subject == null)
+                throw new ValidationException("Need subject");
+            if (TextBody == null && HtmlBody == null)
+                throw new ValidationException("Need body");
+
+
+            MimeMessage msg = new MimeMessage();
+            msg.From.Add(MailboxAddress.Parse(From));
+            List<string> recipients
+                = new List<string>(
+                    (To?.Length ?? 0) +
+                    (Cc?.Length ?? 0) +
+                    (Bcc?.Length ?? 0));
+            if (recipients.Capacity == 0)
+                throw new ValidationException("Need atleast one recipient");
+            foreach (var to in To)
+            {
+                msg.To.Add(MailboxAddress.Parse(to));
+                recipients.Add(to);
+            }
+            if (Cc != null)
+            {
+                foreach (var to in Cc)
+                {
+                    msg.Cc.Add(MailboxAddress.Parse(to));
+                    recipients.Add(to);
+                }
+            }
+            if (Bcc != null)
+            {
+                foreach (var to in Bcc)
+                {
+                    msg.Bcc.Add(MailboxAddress.Parse(to));
+                    recipients.Add(to);
+                }
+            }
+            msg.Subject = Subject;
+
+            if(UnsubscribeLink != null)
+            {
+                msg.Headers.Add(HeaderId.ListUnsubscribe, Encoding.UTF8, $"<{UnsubscribeLink}>");
+            }
+
+            if(Summary != null)
+            {
+                msg.Headers.Add(HeaderId.Summary, Encoding.UTF8, Summary);
+            }
+
+            BodyBuilder bb = new BodyBuilder();
+            if (TextBody != null)
+            {
+                bb.TextBody = TextBody;
+            }
+            if (HtmlBody != null)
+            {
+                bb.HtmlBody = HtmlBody;
+            }
+
+            if(files != null)
+            {
+                foreach (var file in files) {
+                    bb.Attachments.Add(file.FileName, file.OpenReadStream(), ContentType.Parse(file.ContentType));
+                }
+            }   
+
+            msg.Body = bb.ToMessageBody();
+            var ms = new MemoryStream();
+            msg.WriteTo(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+            return (ms, recipients);
+        }
     }
 }
