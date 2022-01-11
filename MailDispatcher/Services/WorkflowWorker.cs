e2175ba0e09b31d84c571fbfb9415216e9147c94
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-01-11 21:39:44 +0530

    Delete old workflow removed

--- MailDispatcher/Services/WorkflowWorker.cs
+++ MailDispatcher/Services/WorkflowWorker.cs
@@ -21,18 +21,18 @@ namespace MailDispatcher.Services
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            Task.Run(async () => {
-                try {
-                    // await workflowService.Storage.DeleteOrphanActivities();
-                    await workflowService.Storage.DeleteOldWorkflows(30);
-                }
-                catch (Exception ex)
-                {
-                    telemetryClient.TrackException(ex);
-                }
-            });
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+//#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+//            Task.Run(async () => {
+//                try {
+//                    // await workflowService.Storage.DeleteOrphanActivities();
+//                    await workflowService.Storage.DeleteOldWorkflows(30);
+//                }
+//                catch (Exception ex)
+//                {
+//                    telemetryClient.TrackException(ex);
+//                }
+//            });
+//#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             while (true)
             {
                 if (stoppingToken.IsCancellationRequested)
